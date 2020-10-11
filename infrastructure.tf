3d207edbc11a98560f550663117a31bc2e084a25
Deepansh Sachdeva <deepansh.99530@gmail.com>
2020-10-10 23:06:51 -0400

    :zap: add subnets, rtb, igw & public route

--- infrastructure.tf
+++ infrastructure.tf
@@ -4,10 +4,84 @@ provider "aws" {
 }
 
 # VPC for infrastructure
-resource "aws_vpc" "tf_vpc" {
+resource "aws_vpc" "csye6225_vpc" {
   cidr_block           = var.cidr_block
   enable_dns_hostnames = true
   tags = {
     "Name" = var.vpc_name
   }
+}
+
+# Subnet 1 for VPC
+resource "aws_subnet" "subnet1" {
+  vpc_id                  = aws_vpc.csye6225_vpc.id
+  cidr_block              = var.cidrs[0]
+  availability_zone       = join("", [var.region, var.azs[0]])
+  map_public_ip_on_launch = true
+  tags = {
+    "Name" = "subnet1"
+  }
+}
+
+# Subnet 2 for VPC
+resource "aws_subnet" "subnet2" {
+  vpc_id                  = aws_vpc.csye6225_vpc.id
+  cidr_block              = var.cidrs[1]
+  availability_zone       = join("", [var.region, var.azs[1]])
+  map_public_ip_on_launch = true
+  tags = {
+    "Name" = "subnet2"
+  }
+}
+
+# Subnet 3 for VPC
+resource "aws_subnet" "subnet3" {
+  vpc_id                  = aws_vpc.csye6225_vpc.id
+  cidr_block              = var.cidrs[2]
+  availability_zone       = join("", [var.region, var.azs[2]])
+  map_public_ip_on_launch = true
+  tags = {
+    "Name" = "subnet3"
+  }
+}
+
+# Internet gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.csye6225_vpc.id
+  tags = {
+    "Name" = "csye6225-igw"
+  }
+}
+
+# Route table
+resource "aws_route_table" "rtb" {
+  vpc_id = aws_vpc.csye6225_vpc.id
+  tags = {
+    "Name" = "csye6225-rtb"
+  }
+}
+
+# Public route
+resource "aws_route" "public_route" {
+  route_table_id         = aws_route_table.rtb.id
+  gateway_id             = aws_internet_gateway.igw.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+# Subnet route table association 1
+resource "aws_route_table_association" "assoc1" {
+  subnet_id      = aws_subnet.subnet1.id
+  route_table_id = aws_route_table.rtb.id
+}
+
+# Subnet route table association 2
+resource "aws_route_table_association" "assoc2" {
+  subnet_id      = aws_subnet.subnet2.id
+  route_table_id = aws_route_table.rtb.id
+}
+
+# Subnet route table association 3
+resource "aws_route_table_association" "assoc3" {
+  subnet_id      = aws_subnet.subnet3.id
+  route_table_id = aws_route_table.rtb.id
 }
--- variables.tf
+++ variables.tf
@@ -17,3 +17,13 @@ variable "cidr_block" {
   description = "CIDR block for VPC."
   default     = "10.0.0.0/16"
 }
+
+variable "cidrs" {
+  description = "CIDR blocks for subnets."
+  default     = ["10.0.0.0/24", "10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "azs" {
+  description = "Availability zones for subnets."
+  default     = ["a", "b", "c"]
+}
